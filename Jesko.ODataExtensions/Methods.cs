18a366f2531c4da3435e3119aa82932e113417c8
Ben Jeske <jeske717@gmail.com>
2013-04-11 18:49:05 -0500

    got first test passing

--- Jesko.ODataExtensions/Methods.cs
+++ Jesko.ODataExtensions/Methods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Jesko.ODataExtensions
 {
@@ -7,7 +8,17 @@ namespace Jesko.ODataExtensions
     {
         public static OData Filter<TResult>(this TResult instance, Expression<Func<TResult, bool>> expression)
         {
-            return new OData();
+            var binaryExpression = expression.Body as BinaryExpression;
+            var left = binaryExpression.Left as MemberExpression;
+            var field = left.Member as PropertyInfo;
+
+            var right = binaryExpression.Right.ToString().Replace("\"", "");
+            return new OData
+                       {
+                           PropertyName = field.Name,
+                           Method = "eq",
+                           Value = right
+                       };
         }
     }
 }
--- Jesko.ODataExtensions/OData.cs
+++ Jesko.ODataExtensions/OData.cs
@@ -2,6 +2,15 @@
 {
     public class OData
     {
-         
+        public string Type { get { return "filter"; }}
+
+        public string PropertyName { get; set; }
+        public string Method { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("${0}({1} {2} '{3}')", Type, PropertyName, Method, Value);
+        }
     }
 }
