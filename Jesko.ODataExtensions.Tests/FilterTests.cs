3fab7093bcb56d1293e0c6fa54b71143fedd9871
Ben Jeske <jeske717@gmail.com>
2013-04-11 20:54:45 -0500

    support more expression types

--- Jesko.ODataExtensions.Tests/FilterTests.cs
+++ Jesko.ODataExtensions.Tests/FilterTests.cs
@@ -16,17 +16,6 @@ namespace Jesko.ODataExtensions.Tests
         }
         
         [Test]
-        public void Filter_Generates_OData_Filter_For_Equality_With_NumericVariable_OnRightSide()
-        {
-            var instance = new ClassWithProperties();
-            const int rightSide = 23;
-
-            var actual = instance.Filter(x => x.IntegerProperty == rightSide);
-
-            Assert.AreEqual("$filter(IntegerProperty eq '23')", actual.ToString());
-        }
-
-        [Test]
         public void Filter_Works_On_Types_Other_Than_The_InstanceType()
         {
             var instance = new OtherClass();
@@ -35,6 +24,56 @@ namespace Jesko.ODataExtensions.Tests
             var actual = instance.Filter<ClassWithProperties>(x => x.IntegerProperty == rightSide);
 
             Assert.AreEqual("$filter(IntegerProperty eq '23')", actual.ToString());
+        }
+
+        [Test]
+        public void Filter_Generates_Inequality_Filter_When_Expression_Represents_Inequality()
+        {
+            var instance = new ClassWithProperties();
+
+            var actual = instance.Filter(x => x.StringProperty != "value");
+
+            Assert.AreEqual("$filter(StringProperty ne 'value')", actual.ToString());
+        }
+
+        [Test]
+        public void Filter_Generates_GreaterThan_Filter_When_Expression_Represents_GreaterThan()
+        {
+            var instance = new ClassWithProperties();
+
+            var actual = instance.Filter(x => x.IntegerProperty > 7);
+
+            Assert.AreEqual("$filter(IntegerProperty gt '7')", actual.ToString());
+        }
+
+        [Test]
+        public void Filter_Generates_GreaterThanOrEqualTo_Filter_When_Expression_Represents_GreaterThanOrEqualTo()
+        {
+            var instance = new ClassWithProperties();
+
+            var actual = instance.Filter(x => x.IntegerProperty >= 7);
+
+            Assert.AreEqual("$filter(IntegerProperty ge '7')", actual.ToString());
+        }
+
+        [Test]
+        public void Filter_Generates_LessThan_Filter_When_Expression_Represents_LessThan()
+        {
+            var instance = new ClassWithProperties();
+
+            var actual = instance.Filter(x => x.IntegerProperty < 7);
+
+            Assert.AreEqual("$filter(IntegerProperty lt '7')", actual.ToString());
+        }
+
+        [Test]
+        public void Filter_Generates_LessThanOrEqualTo_Filter_When_Expression_Represents_LessThanOrEqualTo()
+        {
+            var instance = new ClassWithProperties();
+
+            var actual = instance.Filter(x => x.IntegerProperty <= 7);
+
+            Assert.AreEqual("$filter(IntegerProperty le '7')", actual.ToString());
         }
     }
 
--- Jesko.ODataExtensions/Methods.cs
+++ Jesko.ODataExtensions/Methods.cs
@@ -16,14 +16,34 @@ namespace Jesko.ODataExtensions
             var binaryExpression = expression.Body as BinaryExpression;
             var left = binaryExpression.Left as MemberExpression;
             var field = left.Member as PropertyInfo;
-
             var right = binaryExpression.Right.ToString().Replace("\"", "");
+
             return new OData
                        {
                            PropertyName = field.Name,
-                           Method = "eq",
+                           Method = Method(binaryExpression.NodeType),
                            Value = right
                        };
+        }
+
+        private static string Method(ExpressionType expressionType)
+        {
+            switch (expressionType)
+            {
+                case ExpressionType.Equal:
+                    return "eq";
+                case ExpressionType.NotEqual:
+                    return "ne";
+                case ExpressionType.GreaterThan:
+                    return "gt";
+                case ExpressionType.GreaterThanOrEqual:
+                    return "ge";
+                case ExpressionType.LessThan:
+                    return "lt";
+                case ExpressionType.LessThanOrEqual:
+                    return "le";
+            }
+            return null;
         }
     }
 }
